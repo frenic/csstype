ca0cc0f6950b177419aa6096383e7ef0dc8de5b8
Fredrik Nicol <fredrik.nicol@gmail.com>
2019-11-16 20:05:44 +0100

    Bump Npm packages

--- src/flow.ts
+++ src/flow.ts
@@ -148,7 +148,9 @@ function outputDeclaration(entry: IDeclaration, namespace = '') {
     output += 'export ';
   }
 
-  output += `type ${namespace + entry.name + stringifyGenerics(entry.generics, !entry.export, stringifyTypes)} = ${stringifyTypes(entry.types)}`;
+  output += `type ${namespace +
+    entry.name +
+    stringifyGenerics(entry.generics, !entry.export, stringifyTypes)} = ${stringifyTypes(entry.types)}`;
 
   return output;
 }
