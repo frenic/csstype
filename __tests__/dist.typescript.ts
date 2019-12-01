5d33879c7b39d04f096c481b977d63cd4ace1204
Fredrik Nicol <fredrik.nicol@gmail.com>
2019-12-01 20:31:15 +0100

    Add tests with multiple versions av Typescript

--- __tests__/dist.typescript.ts
+++ __tests__/dist.typescript.ts
@@ -1,23 +1,60 @@
 import * as path from 'path';
-import * as ts from 'typescript';
 
-test('it detects errors', () => {
-  const errors = typescriptDiagnostic([path.resolve(__dirname, '__fixtures__/typecheck.ts')], {
-    noEmit: true,
-    strict: true,
+const COMPILER_OPTIONS = {
+  noEmit: true,
+  strict: true,
+};
+
+describe('Typescript 3.7', () => {
+  it('detects errors', async () => {
+    const ts = await import('typescript');
+    const program = ts.createProgram([path.resolve(__dirname, '__fixtures__/typecheck.ts')], COMPILER_OPTIONS);
+    const diagnostics = ts.getPreEmitDiagnostics(program);
+    const errors = diagnostics.map(diagnostic => {
+      const { line, character } = diagnostic.file!.getLineAndCharacterOfPosition(diagnostic.start!);
+      return `${line}:${character} - ${ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')}`;
+    });
+
+    expect(Number(ts.versionMajorMinor)).toBe(3.7);
+    expect(errors.length).toBe(14);
+    for (const error of errors) {
+      expect(error).toMatchSnapshot();
+    }
   });
-
-  expect(errors.length).toBe(14);
-  for (const error of errors) {
-    expect(error).toMatchSnapshot();
-  }
 });
 
-function typescriptDiagnostic(fileName: string[], options: ts.CompilerOptions) {
-  const program = ts.createProgram(fileName, options);
-  const diagnostics = ts.getPreEmitDiagnostics(program);
-  return diagnostics.map(diagnostic => {
-    const { line, character } = diagnostic.file!.getLineAndCharacterOfPosition(diagnostic.start!);
-    return `${line}:${character} - ${ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')}`;
+describe('Typescript 3.6', () => {
+  it('detects errors', async () => {
+    const ts = await import('typescript3.6');
+    const program = ts.createProgram([path.resolve(__dirname, '__fixtures__/typecheck.ts')], COMPILER_OPTIONS);
+    const diagnostics = ts.getPreEmitDiagnostics(program);
+    const errors = diagnostics.map(diagnostic => {
+      const { line, character } = diagnostic.file!.getLineAndCharacterOfPosition(diagnostic.start!);
+      return `${line}:${character} - ${ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')}`;
+    });
+
+    expect(Number(ts.versionMajorMinor)).toBe(3.6);
+    expect(errors.length).toBe(14);
+    for (const error of errors) {
+      expect(error).toMatchSnapshot();
+    }
   });
-}
+});
+
+describe('Typescript 3.5', () => {
+  it('detects errors', async () => {
+    const ts = await import('typescript3.5');
+    const program = ts.createProgram([path.resolve(__dirname, '__fixtures__/typecheck.ts')], COMPILER_OPTIONS);
+    const diagnostics = ts.getPreEmitDiagnostics(program);
+    const errors = diagnostics.map(diagnostic => {
+      const { line, character } = diagnostic.file!.getLineAndCharacterOfPosition(diagnostic.start!);
+      return `${line}:${character} - ${ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n')}`;
+    });
+
+    expect(Number(ts.versionMajorMinor)).toBe(3.5);
+    expect(errors.length).toBe(14);
+    for (const error of errors) {
+      expect(error).toMatchSnapshot();
+    }
+  });
+});
