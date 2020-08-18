0dd71f9bb04dc6c9dc969fc79e68500c5d281ec3
Fredrik Nicol <fredrik.nicol@gmail.com>
2020-08-18 10:53:40 +0200

    Bump Typescript@4.0-rc

--- __tests__/dist.typescript.ts
+++ __tests__/dist.typescript.ts
@@ -5,9 +5,29 @@ const COMPILER_OPTIONS = {
   strict: true,
 };
 
-describe('Typescript 3.9', () => {
+describe('Typescript 4.0', () => {
   it('detects errors', async () => {
     const ts = await import('typescript');
+    const program = ts.createProgram([path.resolve(__dirname, '__fixtures__/typecheck.ts')], COMPILER_OPTIONS);
+    const diagnostics = ts.getPreEmitDiagnostics(program);
+    const errors = diagnostics.map(diagnostic => {
+      const { line, character } = diagnostic.file!.getLineAndCharacterOfPosition(diagnostic.start!);
+      return `${line}:${character} - ${removeAbsolutePaths(
+        ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'),
+      )}`;
+    });
+
+    expect(Number(ts.versionMajorMinor)).toBe(4.0);
+    expect(errors.length).toBe(15);
+    for (const error of errors) {
+      expect(error).toMatchSnapshot();
+    }
+  });
+});
+
+describe('Typescript 3.9', () => {
+  it('detects errors', async () => {
+    const ts = await import('typescript3.9');
     const program = ts.createProgram([path.resolve(__dirname, '__fixtures__/typecheck.ts')], COMPILER_OPTIONS);
     const diagnostics = ts.getPreEmitDiagnostics(program);
     const errors = diagnostics.map(diagnostic => {
