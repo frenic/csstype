0abcbfbc44c3f97c2379d18236d0fe78647c74ff
Artem Malko <artem.malko@gmail.com>
2020-11-23 13:14:50 +0700

    Add content-visibility to patched props

--- src/data/patches.ts
+++ src/data/patches.ts
@@ -9,6 +9,22 @@ export const properties: { [property: string]: IExtendedProperty } = {
   'line-clamp': {
     shorthand: true,
   },
+
+  /**
+   * w3c working draft https://www.w3.org/TR/css-contain-2/#content-visibility
+   * caniuse https://caniuse.com/css-content-visibility
+   */
+  'content-visibility': {
+    shorthand: false,
+    syntax: 'visible | auto | hidden',
+    initial: 'visible',
+    inherited: false,
+    animationType: 'discrete',
+    percentages: 'n/a',
+    appliesto: 'elements for which layout containment can apply',
+    computed: 'as specified',
+    status: 'Working Draft',
+  },
 };
 
 export const syntaxes: { [property: string]: MDN.Syntax } = {
