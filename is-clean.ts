9c4a2a95d0133c0974a1553b955c2be46c0018bb
Fredrik Nicol <fredrik.nicol@gmail.com>
2025-11-17 14:00:13 +0100

    Fix CI

--- is-clean.ts
+++ is-clean.ts
@@ -3,6 +3,6 @@ import { spawnSync } from 'child_process';
 const result = spawnSync('git', ['status', '--porcelain'], { encoding: 'utf-8' });
 
 if (result.stdout !== '') {
-  console.error('Your working directory needs to be clean!');
+  console.error('Your working directory is not clean!');
   process.exit(1);
 }
